e38e164dba14cd7383a4109c626ed7ab7cf13bf5
Tamara Temple <tamouse@gmail.com>
2010-08-20 16:14:06 -0500

    Added feature that would check to see if a config.inc.php file exists. This is so that a sample config file can be saved, but it won't overwrite an existing config file if one exists for the site.
     On branch experimental
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	new file:   .gitignore
    	renamed:    config.inc.php -> config.inc.php-sample
    	modified:   defaultincludes.inc
    	new file:   noconfig.php

---
+++ noconfig.php
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+	"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<title>No local configuration file!</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="generator" content="TextMate" />
+	<meta name="author" content="Tamara Temple" />
+	<meta name="copyright" content"Copyright &copy; 2010 by Tamara Temple. All rights reserved." />
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+
+
+	
+</head>
+
+<body id="noconfig" title="noconfig" lang="en-US">
+
+<div id="header">
+	<div id="topnav"></div>
+
+</div>
+
+<div id="title">
+	<h1><script type="text/javascript" charset="utf-8">
+		document.writeln(document.title);
+	</script></h1>
+</div>
+
+<div id="author"></div>
+
+<div id="content">
+<h3>No local configuration file exists!</h3>
+<p>You must copy config.inc.php-sample to config.inc.php in the root directory of the application and modify it according to your needs.</p>
+</div>
+
+<div id="footer">
+	<div id="copyright">
+		Copyright &copy; 2010 Tamara Temple. All rights reserved.
+	</div>
+</div>
+
+</body>
+</html>
